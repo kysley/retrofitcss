7c693ddb7e94ecf3e441f9f50a952abbb1dcc66b
Evan Kysley <e.kysley@gmail.com>
2017-01-03 00:11:48 -0500

    Removed unused lines

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,7 +7,6 @@ var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
 var comment = '\/*\r\n* RETROFIT ' + pkg.version + '\r\n* Copyright 2016, Evan Kysley\r\n* http:\/\/retrofit.space\/\r\n* Free to use under the MIT license.\r\n* https:\/\/kysley.github.io\/license\r\n*\/\r\n';
 var $ = require('gulp-load-plugins')();
-// var pug = require('gulp-pug');
 
 gulp.task('watch', function() {
   gulp.watch('./**/*.scss', ['build']);
@@ -32,33 +31,4 @@ gulp.task('minify', function() {
     .pipe(gulp.dest('./dist'))
 })
 
-// gulp.task('pug', function() {
-//     return gulp.src('*.pug')
-//     .pipe(pug({pretty: true}))
-//     .pipe(gulp.dest('./'));
-// });
-
 gulp.task('default', ['build', 'minify']);
-
-// gulp.task('build', function () {
-// 	return gulp.src(['./src/toc.css', './src/base.css', './src/typography.css', './src/grid.css', './src/links.css', './src/buttons.css', './src/lists.css', './src/forms.css', './src/misc.css'])
-// 		.pipe($.concat('retrofit.css'))
-// 		.pipe($.header(comment + '\n'))
-// 		.pipe($.size())
-// 		.pipe(gulp.dest('./dist/'));
-// });
-
-// gulp.task('minify', ['build'], function() {
-//   return gulp.src(['./dist/retrofit.css'])
-//     .pipe(minifyCSS())
-//     .pipe($.header(comment))
-//     .pipe($.size())
-//     .pipe($.concat('retrofit.min.css'))
-//     .pipe(gulp.dest('./dist/'));
-// });
-
-// gulp.task('watch', function() {
-//   gulp.watch(['src/*.css'], ['default']);
-// });
-
-// gulp.task('default', ['build', 'minify']);
